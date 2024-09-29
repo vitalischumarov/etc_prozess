0d3c877c7ab2271b8d2a58fde6bec8818e18d1b9
Vitali Schumarov <vitalischumarov@696ff83f-e626-48e0-9d11-267d06ed5a3f.fritz.box>
2024-09-29 21:54:22 +0200

    load .csv files

--- Code/exctract.py
+++ Code/exctract.py
@@ -1,1 +1,5 @@
-import pandas
+import pandas as pd
+
+df1 = pd.read_csv('/Users/vitalischumarov/Desktop/Data Engineering/Python/Code/ETL_CSV/Source/data1.csv')
+df2 = pd.read_csv('/Users/vitalischumarov/Desktop/Data Engineering/Python/Code/ETL_CSV/Source/data2.csv')
+df3 = pd.read_csv('/Users/vitalischumarov/Desktop/Data Engineering/Python/Code/ETL_CSV/Source/data3.csv')
