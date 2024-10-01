482eb14bbe032a281b800948ab9811ba7fa2e168
Vitali Schumarov <vitalischumarov@Vitalis-Mac-mini.fritz.box>
2024-10-01 20:18:03 +0200

    bug fixes

--- Pipeline/loading.py
+++ Pipeline/loading.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 df = pd.read_csv('./Staging_Area/staging_areaDB.csv')
 
-with open('./Data_Warehouse/data_warehouse.csv','x') as file:
+
+
+with open('./Data_Warehouse/data_warehouse.csv','w') as file:
     df.to_csv('./Data_Warehouse/Data_Warehouse.csv', index=False)
     print('successfully loaded into data warehouse')
 
